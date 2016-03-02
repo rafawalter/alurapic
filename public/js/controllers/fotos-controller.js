71f52ff7662239acbeba01aa69fe8034379f9f36
Rafael Walter <rafawalter@gmail.com>
2016-03-02 20:09:38 -0300

    remover foto

--- public/js/controllers/fotos-controller.js
+++ public/js/controllers/fotos-controller.js
@@ -7,4 +7,17 @@ angular.module("alurapic").controller("FotosController", function($scope, $http)
 		});
 
 	$scope.filtro = '';
+	$scope.mensagem = '';
+
+	$scope.remove = function(foto) {
+		$http.delete('/v1/fotos/' + foto._id)
+			.success(function() {
+				$scope.mensagem = 'foto '+foto.titulo+' removida com sucesso';
+				var indice = $scope.fotos.indexOf(foto);
+				$scope.fotos.splice(indice, 1);
+			}).error(function(erro) {
+				console.log(erro);
+				$scope.mensagem = 'Não foi possível remover a foto '+foto.titulo;
+			});
+	};
 });
