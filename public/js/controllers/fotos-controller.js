d93347995639cf24840dfee75a344f66aebde8b4
Rafael Walter <rafawalter@gmail.com>
2016-03-03 18:45:53 -0300

    ""

--- public/js/controllers/fotos-controller.js
+++ public/js/controllers/fotos-controller.js
@@ -1,10 +1,33 @@
-angular.module("alurapic").controller("FotosController", function($scope, $http) {
+angular.module("alurapic").controller("FotosController", function($scope, $resource) {
 	$scope.init = function() {
 		$scope.obterFotos();
 		$scope.filtro = '';
 		$scope.mensagem = '';		
+		$scope.fotos = [];
 	};
 
+
+	var recursoFoto = $resource('/v1/fotos/:fotoId');
+	$scope.obterFotos = function() {
+		recursoFoto.query(function(fotos) {
+			$scope.fotos = fotos;
+		}, function(erro) {
+			console.log(erro);
+		});
+	};
+
+	$scope.remove = function(foto) {
+		recursoFoto.delete({fotoId : foto._id}, function() {
+			$scope.mensagem = 'foto '+foto.titulo+' removida com sucesso';
+			var indice = $scope.fotos.indexOf(foto);
+			$scope.fotos.splice(indice, 1);
+		}, function(erro) {
+			console.log(erro);
+			$scope.mensagem = 'Não foi possível remover a foto '+foto.titulo;
+
+		});
+	};
+/*
 	$scope.obterFotos = function() {
 		$http.get('/v1/fotos')
 			.success(function(fotos) {
@@ -13,7 +36,9 @@ angular.module("alurapic").controller("FotosController", function($scope, $http)
 				console.log(erro);
 			});
 	};
+*/
 
+/*
 	$scope.remove = function(foto) {
 		$http.delete('/v1/fotos/' + foto._id)
 			.success(function() {
@@ -25,6 +50,6 @@ angular.module("alurapic").controller("FotosController", function($scope, $http)
 				$scope.mensagem = 'Não foi possível remover a foto '+foto.titulo;
 			});
 	};
-
+*/
 	$scope.init();
 });
